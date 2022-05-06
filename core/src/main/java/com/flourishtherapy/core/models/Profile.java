3c29d6d184deb67426b2ffb94216e2348969ef6b
Shehjad Khan <shehjad.khan@ansys.com>
2022-05-05 19:28:30 -0500

    Added profile preview component

---
+++ core/src/main/java/com/flourishtherapy/core/models/Profile.java
@@ -0,0 +1,30 @@
+package com.flourishtherapy.core.models;
+
+
+public class Profile {
+
+	String name;
+	String description;
+	String profileImagePath;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getProfileImagePath() {
+		return profileImagePath;
+	}
+	public void setProfileImagePath(String profileImagePath) {
+		this.profileImagePath = profileImagePath;
+	}
+	
+
+}
